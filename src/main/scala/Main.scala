5469058fe332f30d28144dc8bb9e8de636ee09ae
nanothermite <hk-vndr@iscs-i.com>
2018-07-01 00:39:34 -0400

    Refine logic for all combos of symmetrical halves, overflow to next wider value.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,18 +3,30 @@ import scala.collection.mutable.ArrayBuffer
 
 object Main extends App {
   var inputs = ArrayBuffer[String]()
-  for (ln <- io.Source.stdin.getLines()) {
+/*  for (ln <- io.Source.stdin.getLines()) {
     inputs.append(ln)
-  }
-  val numVals = inputs(0).toInt
-  val numSeq = inputs.tail.toList
+  }*/
+  val numVals = 3//inputs(0).toInt
+  val numSeq = List("999", "81008", "808", "2133") //inputs.tail.toList
 
   numSeq.foreach(sym => println(nextSym(sym)))
 
   def nextSym(num: String) : String = {
     val (l, r) = lr(num)
-    val oneSide = compareLR(l, r.reverse, l.length - 1)
-    oneSide + (if (num.length % 2 == 0) oneSide.reverse else  oneSide.reverse.drop(1))
+    val odd = num.length % 2 == 1
+    // symmetrical pairs need to be bumped up to next value
+    val oneSide = if (l == r.reverse) (BigInt(l) + 1).toString else compareLR(l, r.reverse, l.length - 1)
+    val expanded = oneSide.length > l.length
+
+  // logic of combining the returned half:
+  // if (even and not expanded):  oneSide + oneSide.reverse
+  // if (even and expanded):      oneside + oneSide.reverse.drop(1)
+  // if (odd and not expanded):   oneside + oneSide.reverse.drop(1)
+  // if (odd and expanded):       oneside.take(oneSide.length - 1) + oneSide.reverse.drop(1)
+
+    if (!odd && !expanded) oneSide + oneSide.reverse
+    else if(odd && expanded) oneSide.take(oneSide.length - 1) + oneSide.reverse.drop(1)
+    else oneSide + oneSide.reverse.drop(1)
   }
 
   def lr(inp: String): (String, String) = {
@@ -30,7 +42,7 @@ object Main extends App {
     if (i < 0) left
     else {
       if (left(i) > right(i)) left
-      else if (left(i) <= right(i)) (BigInt.apply(left) + 1).toString
+      else if (left(i) < right(i)) (BigInt.apply(left) + 1).toString
       else compareLR(left, right, i - 1)
     }
   }
